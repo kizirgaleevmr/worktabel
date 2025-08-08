d00dd93578cefcb580f8cbff9c74e202802df119
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-08 16:33:38 +0500

    Добавил домашнюю страницу

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+    return (
+        <section id="home" className="max-w-7xl px-2">
+            Home page
+        </section>
+    );
+};
