09ee5a5e594c56d07cf3cdc3df3ae1d3bb8a8c8e
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-19 01:14:59 +0500

    Добавил форму входа

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,106 @@
+import { useNavigate, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { signInUser } from "../../firebase";
+import { startSession } from "../../storage/session";
+
 export const Login = () => {
+    const navigate = useNavigate();
+    const [error, setError] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (event) => {
+        event?.preventDefault();
+        // let navigate = useNavigate();
+
+        // validate the inputs
+        if (!email || !password) {
+            setError("Please enter your username and password.");
+            return;
+        }
+
+        // clear the errors
+        setError("");
+
+        // TODO: send the login request
+        try {
+            let loginResponse = await signInUser(email, password);
+            startSession(loginResponse.user);
+            navigate("/home", { replace: true });
+            console.log("Logging in...");
+        } catch (error) {
+            console.error(error.message);
+            setError(error.message);
+        }
+    };
+
     return (
-        <>
-            <h3>Login</h3>
-        </>
+        <section>
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Вход
+                        </h1>
+                        <form
+                            className="space-y-4 md:space-y-6"
+                            action="#"
+                            onSubmit={handleSubmit}
+                        >
+                            <div>
+                                <label
+                                    htmlFor="email"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    email
+                                </label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="name@company.com"
+                                    required=""
+                                />
+                            </div>
+                            <div>
+                                <label
+                                    htmlFor="password"
+                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                >
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    onChange={(e) =>
+                                        setPassword(e.target.value)
+                                    }
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required=""
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-700 dark:hover:bg-sky-800 cursor-pointer"
+                            >
+                                Войти
+                            </button>
+                            <div className="text-right">
+                                <Link
+                                    to="/register"
+                                    className=" text-cyan-400 dark:hover:text-amber-300"
+                                >
+                                    Регистрация
+                                </Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
