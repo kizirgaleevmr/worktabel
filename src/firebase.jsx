b0ee265e7a7e9299fb367d65b2e94320b58b69e4
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-18 19:56:19 +0500

    Добавил данные firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAB0iZdcmr8DI54qGhuUqpNf_OFcGgGomQ",
+    authDomain: "worktable-2e9f2.firebaseapp.com",
+    projectId: "worktable-2e9f2",
+    storageBucket: "worktable-2e9f2.firebasestorage.app",
+    messagingSenderId: "704223310557",
+    appId: "1:704223310557:web:60353f38a7f97d15752134",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
