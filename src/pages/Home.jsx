6b9450e09b579b5fdf02de5e8db5a9fc8f815c4e
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-19 22:36:25 +0500

    Изменил домашнюю страницу

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { DefaultSidebar } from "../components/ui/Sidebar";
+import { Outlet, Route, Routes, useLocation } from "react-router-dom";
+import { Tabel } from "./Tabel";
+
+const route = [{ path: "/home/tabel", element: <Tabel /> }];
 
 export const Home = () => {
+    const location = useLocation();
+    console.log(location);
     return (
         <section id="home" className="max-w-7xl px-2">
-            Home page
+            <DefaultSidebar />
         </section>
     );
 };
