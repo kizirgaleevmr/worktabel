4272c011ea307cfddbbe75b6c85656b57c14bb3c
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-19 22:37:14 +0500

    Добавил outlet  для вложенных страниц

--- src/components/ui/Sidebar.jsx
+++ src/components/ui/Sidebar.jsx
@@ -14,57 +14,77 @@ import {
     Cog6ToothIcon,
     InboxIcon,
     PowerIcon,
+    BookOpenIcon,
+    UserGroupIcon,
+    PhoneArrowDownLeftIcon,
+    UserIcon,
 } from "@heroicons/react/24/solid";
+import { Link, Outlet, Route, Routes } from "react-router-dom";
+import { Tabel } from "../../pages/Tabel";
+import { Users } from "../../pages/Users";
 
 export function DefaultSidebar() {
     return (
-        <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 text-2xl">
-            <div className="mb-2 p-4">
-                <Typography variant="h5" color="blue-gray">
-                    Меню
-                </Typography>
+        <div className="flex">
+            <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5 text-2xl mr-6">
+                <div className="mb-2 p-4">
+                    <Typography variant="h5" color="blue-gray">
+                        Меню
+                    </Typography>
+                </div>
+                <List>
+                    <Link to="tabel">
+                        <ListItem className="mb-3 cursor-pointer">
+                            <ListItemPrefix>
+                                <BookOpenIcon className="h-5 w-5 mr-4" />
+                            </ListItemPrefix>
+                            Табыль времени
+                        </ListItem>
+                    </Link>
+                    <Link to="sotrudniki">
+                        <ListItem className="mb-3 cursor-pointer">
+                            <ListItemPrefix>
+                                <UserGroupIcon className="h-5 w-5 mr-4" />
+                            </ListItemPrefix>
+                            Сотрудники
+                        </ListItem>
+                    </Link>
+                    <Link to="message">
+                        <ListItem className="mb-3 cursor-pointer">
+                            <ListItemPrefix>
+                                <PhoneArrowDownLeftIcon className="h-5 w-5 mr-4" />
+                            </ListItemPrefix>
+                            Заявки
+                            <ListItemSuffix>
+                                <Chip
+                                    value="14"
+                                    size="sm"
+                                    variant="ghost"
+                                    color="blue-gray"
+                                    className="rounded-full"
+                                />
+                            </ListItemSuffix>
+                        </ListItem>
+                    </Link>
+                    <Link to="profile">
+                        <ListItem className="mb-3 cursor-pointer">
+                            <ListItemPrefix>
+                                <UserIcon className="h-5 w-5 mr-4" />
+                            </ListItemPrefix>
+                            Профиль
+                        </ListItem>
+                    </Link>
+                    <ListItem className="mb-3 cursor-pointer">
+                        <ListItemPrefix>
+                            <PowerIcon className="h-5 w-5 mr-4" />
+                        </ListItemPrefix>
+                        Выйти
+                    </ListItem>
+                </List>
+            </Card>
+            <div>
+                <Outlet />
             </div>
-            <List>
-                <ListItem className="mb-3">
-                    <ListItemPrefix>
-                        <PresentationChartBarIcon className="h-5 w-5 mr-4" />
-                    </ListItemPrefix>
-                    Табыль времени
-                </ListItem>
-                <ListItem className="mb-3">
-                    <ListItemPrefix>
-                        <ShoppingBagIcon className="h-5 w-5 mr-4" />
-                    </ListItemPrefix>
-                    Сотрудники
-                </ListItem>
-                <ListItem className="mb-3">
-                    <ListItemPrefix>
-                        <InboxIcon className="h-5 w-5 mr-4" />
-                    </ListItemPrefix>
-                    Заявки
-                    <ListItemSuffix>
-                        <Chip
-                            value="14"
-                            size="sm"
-                            variant="ghost"
-                            color="blue-gray"
-                            className="rounded-full"
-                        />
-                    </ListItemSuffix>
-                </ListItem>
-                <ListItem className="mb-3">
-                    <ListItemPrefix>
-                        <Cog6ToothIcon className="h-5 w-5 mr-4" />
-                    </ListItemPrefix>
-                    Профиль
-                </ListItem>
-                <ListItem className="mb-3">
-                    <ListItemPrefix>
-                        <PowerIcon className="h-5 w-5 mr-4" />
-                    </ListItemPrefix>
-                    Выйти
-                </ListItem>
-            </List>
-        </Card>
+        </div>
     );
 }
