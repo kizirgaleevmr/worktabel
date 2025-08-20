e4071c107e04f82ace4a79b3da99f3b4354a246e
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-20 21:18:27 +0500

    Добавил группу кнопок

---
+++ src/components/ui/Button.jsx
@@ -0,0 +1,34 @@
+import { ButtonGroup, Button } from "@material-tailwind/react";
+import { AddProductDialog } from "./ModalUser";
+import React from "react";
+
+export function ButtonGroupColors({ title1, title2, title3 }) {
+    const [isOpen, setOpen] = React.useState(false);
+
+    const handleAddProduct = () => {
+        setOpen(true); // Logic to handle adding a product
+    };
+
+    return (
+        <div className="flex w-max flex-col gap-4">
+            <ButtonGroup
+                color="green"
+                className="bg-green-500 text-white border-radius-3xl"
+            >
+                <Button
+                    className="hover:bg-sky-700 p-2 cursor-pointer"
+                    onClick={handleAddProduct}
+                >
+                    {title1}
+                </Button>
+                <Button className="hover:bg-sky-700 p-2 cursor-pointer">
+                    {title2}
+                </Button>
+                <Button className="hover:bg-sky-700 p-2 cursor-pointer">
+                    {title3}
+                </Button>
+            </ButtonGroup>
+            <AddProductDialog open={isOpen} setOpen={setOpen} />
+        </div>
+    );
+}
