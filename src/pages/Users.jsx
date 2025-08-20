e2ba6c485ee0dc401971bf22fffca0226997f47a
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-20 21:16:32 +0500

    Добавил меню для управлением сотрудников

---
+++ src/pages/Users.jsx
@@ -0,0 +1,13 @@
+import { ButtonGroupColors } from "../components/ui/Button";
+
+export const Users = () => {
+    return (
+        <>
+            <ButtonGroupColors
+                title1={"Добавить сотрудника"}
+                title2={"Удалить сотрудника"}
+                title3={"Посмотреть всех"}
+            />
+        </>
+    );
+};
