4cfa48001a4785d98928d107d7cd52d28d0ebf7f
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-21 23:28:00 +0500

    ИСправил класс

--- src/components/ui/Cards.jsx
+++ src/components/ui/Cards.jsx
@@ -19,7 +19,7 @@ export function HorizontalCard({
     jobDate,
 }) {
     return (
-        <Card className="w-full max-w-[38rem] flex-row text-left rounded-lg overflow-hidden mb-6">
+        <Card className="max-w-[38rem] flex-row text-left rounded-lg overflow-hidden mb-6">
             <CardHeader className="m-0 w-2/5 shrink-0 rounded-r-none">
                 <img
                     src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
