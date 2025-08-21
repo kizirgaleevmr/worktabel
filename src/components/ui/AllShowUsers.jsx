0ca09f36d478c5e41e4e14f537a216425996a017
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-21 22:20:43 +0500

    Добавил компонет показывающий всех сотрудников

---
+++ src/components/ui/AllShowUsers.jsx
@@ -0,0 +1,39 @@
+import { Button } from "@material-tailwind/react";
+import { HorizontalCard } from "./Cards";
+import firebase from "firebase/compat/app";
+export const AllShowUsers = ({ show = "true", setShow }) => {
+    const firstName = "bob",
+        lastName = "ggg",
+        surname = "wew",
+        phone = "8-996-256-63-13",
+        dolzhnost = "rrrr",
+        otdel = "ДКИС",
+        email = "mkr@mail.ru",
+        jobDate = "12213213";
+
+    return (
+        <div className={` ${show ? "active__show_users" : "none__show_users"}`}>
+            <section className="content__users flex flex-row">
+                <HorizontalCard
+                    otdel={otdel}
+                    firstName={firstName}
+                    lastName={lastName}
+                    surname={surname}
+                    phone={phone}
+                    dolzhnost={dolzhnost}
+                    email={email}
+                    jobDate={jobDate}
+                />
+            </section>
+            <Button
+                variant="outlined"
+                className="bg-emerald-500 p-2 text-white border-0"
+                onClick={() => {
+                    setShow(false);
+                }}
+            >
+                Закрыть
+            </Button>
+        </div>
+    );
+};
