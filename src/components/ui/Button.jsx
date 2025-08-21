052b22a76185390a502083075c640f49cf047c81
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-21 23:27:36 +0500

    Добавил событие на получение данных из базы

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,23 +1,29 @@
 import { ButtonGroup, Button } from "@material-tailwind/react";
 import { AddUserDialog } from "./ModalUser";
 import { AllShowUsers } from "./AllShowUsers";
-import React from "react";
+import React, { useState } from "react";
+import { fetchUsers } from "../config/firebase";
 
 export function ButtonGroupColors({ title1, title2 }) {
     //для открытия модалки для добавления пользователя
     const [isOpen, setOpen] = React.useState(false);
-    //для просмотра всх пользователей
+    //для просмотра всех пользователей
     const [isOpenUsers, setOpenUsers] = React.useState(false);
-
+    const [data, setData] = useState();
     //
     const handleAddUsers = () => {
         setOpen(true); // Logic to handle adding a product
     };
     //
+    async function fetchClick() {
+        const data = await fetchUsers();
+        setData(data);
+    }
+    //
     const allShowUsers = () => {
+        fetchClick();
         setOpenUsers(true); // Logic to handle adding a product
     };
-
     return (
         <>
             <div className="flex w-max flex-col gap-4 mb-4">
@@ -40,7 +46,11 @@ export function ButtonGroupColors({ title1, title2 }) {
                 </ButtonGroup>
             </div>
             <AddUserDialog open={isOpen} setOpen={setOpen} />
-            <AllShowUsers show={isOpenUsers} setShow={setOpenUsers} />
+            <AllShowUsers
+                show={isOpenUsers}
+                setShow={setOpenUsers}
+                data={data}
+            />
         </>
     );
 }
