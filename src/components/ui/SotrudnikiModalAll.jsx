c0229e30c39fd8605f32cf7a6f499632ca85c8c7
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-22 23:59:51 +0500

    Переименовал файл с Button на SotrudnikiModalAll

---
+++ src/components/ui/SotrudnikiModalAll.jsx
@@ -0,0 +1,44 @@
+import { ButtonGroup, Button } from "@material-tailwind/react";
+import { AddUserDialog } from "./ModalUser";
+import { AllShowUsers } from "./AllShowUsers";
+import React, { useState } from "react";
+
+export function ButtonGroupColors({ title1, title2 }) {
+    //для открытия модалки для добавления пользователя
+    const [isOpen, setOpen] = React.useState(false);
+    //состоние для показа компонента на странице с карточками
+    const [isOpenUsers, setOpenUsers] = React.useState(false);
+    //для открытия модалки с формой для сотрудников
+    const handleAddUsers = () => {
+        setOpen(true);
+    };
+    //передаем состояние true для отображения компонента AllShowUsers
+    const allShowUsers = () => {
+        setOpenUsers(true);
+    };
+    return (
+        <>
+            <div className="flex w-max flex-col gap-4 mb-4">
+                <ButtonGroup
+                    color="green"
+                    className="bg-green-500 text-white border-radius-3xl gap-x-6"
+                >
+                    <Button
+                        className="hover:bg-sky-700 p-2 cursor-pointer"
+                        onClick={handleAddUsers}
+                    >
+                        {title1}
+                    </Button>
+                    <Button
+                        className="hover:bg-sky-700 p-2 cursor-pointer"
+                        onClick={allShowUsers}
+                    >
+                        {title2}
+                    </Button>
+                </ButtonGroup>
+            </div>
+            <AddUserDialog open={isOpen} setOpen={setOpen} />
+            <AllShowUsers show={isOpenUsers} setShow={setOpenUsers} />
+        </>
+    );
+}
