b578196525019531aed0da56e70680fd90b0adc7
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-08-23 00:05:55 +0500

    Исправил компонент Cards

--- src/components/ui/Cards.jsx
+++ src/components/ui/Cards.jsx
@@ -10,40 +10,62 @@ export function HorizontalCard({
     firstName,
     lastName,
     surname,
-    age,
     dolzhnost,
     phone,
     email,
     src,
     otdel,
-    jobDate,
+    birthday,
+    workDate,
+    id,
 }) {
     return (
-        <Card className="max-w-[38rem] flex-row text-left rounded-lg overflow-hidden mb-6">
-            <CardHeader className="m-0 w-2/5 shrink-0 rounded-r-none">
-                <img
-                    src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
-                    alt="card-image"
-                    className="h-full w-full object-cover"
-                />
+        <Card className="w-[40rem] flex-row text-left rounded-lg overflow-hidden mb-6">
+            <CardHeader
+                className="m-0 w-2/5 shrink-0 rounded-r-none flex items-top justify-center mt-10 flex-col items-center"
+                key={id}
+            >
+                <div className="border-2 border-gray-400 overflow-hidden h-50 w-50 rounded-full flex items-center mb-5">
+                    <img
+                        src={src}
+                        alt="card-image"
+                        className="w-full h-full object-cover"
+                    />
+                </div>
+                <div>
+                    <div className="flex mb-2 flex-col">
+                        <div className="flex items-center mb-8 h-2 w-1/2">
+                            <span className="inline-block mr-3">
+                                {icons.phone}
+                            </span>
+                            <a href={`tel: ${phone}`}>{phone}</a>
+                        </div>
+                        <div className="flex items-center h-2">
+                            <span className="inline-block mr-3">
+                                {icons.email}
+                            </span>
+                            <a href={`mailto:${email}`}>{email}</a>
+                        </div>
+                    </div>
+                </div>
             </CardHeader>
             <CardBody className="w-full">
                 <Typography variant="small" color="gray" className="mb-2">
-                    Отдел: {otdel}{" "}
+                    Отдел: {otdel}
                 </Typography>
                 <Typography variant="small" color="gray" className="mb-2">
                     Табельный №:
                 </Typography>
-                <Typography variant="h4" color="blue-gray" className="mb-2">
-                    Фамилия: {lastName}
+                <Typography variant="h2" color="blue-gray" className="mb-2">
+                    <p className="text-2xl"> {lastName}</p>
                 </Typography>
-                <Typography variant="h4" color="blue-gray" className="mb-2">
-                    Фамилия: {lastName}
+                <Typography variant="h2" color="blue-gray" className="mb-2">
+                    <p className="text-2xl">{firstName}</p>
                 </Typography>
-                <Typography variant="h4" color="blue-gray" className="mb-4">
-                    Фамилия: {lastName}
+                <Typography variant="h2" color="blue-gray" className="mb-4">
+                    <p className="text-2xl">{surname}</p>
                 </Typography>
-                <div className="flex mb-4">
+                <div className="flex mb-4 flex-col">
                     <Typography
                         variant="h2"
                         color="blue-gray"
@@ -56,24 +78,17 @@ export function HorizontalCard({
                         color="blue-gray"
                         className="mb-2 w-1/2"
                     >
-                        стаж: {jobDate}
+                        Возраст: {birthday}
+                    </Typography>
+                    <Typography
+                        variant="h2"
+                        color="blue-gray"
+                        className="mb-2 w-1/2"
+                    >
+                        Стаж: {workDate}
                     </Typography>
                 </div>
                 <div className="text-left">
-                    <div className="flex mb-8 flex-col">
-                        <div className="flex items-center mb-4">
-                            <span className="inline-block mr-3">
-                                {icons.phone}
-                            </span>
-                            <a href={`tel: ${phone}`}>{phone}</a>
-                        </div>
-                        <div className="flex items-center">
-                            <span className="inline-block mr-3">
-                                {icons.email}
-                            </span>
-                            <a href={`mailto:${email}`}>{email}</a>
-                        </div>
-                    </div>
                     <div className="flex justify-between">
                         <div className=" text-gray-400  flex items-center hover:text-cyan-500 cursor-pointer">
                             <span className="inline-block mr-2">
