006120c2a65605672ba01476cc0acaf989a6b9ff
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-09-03 15:43:48 +0500

    добавил стили для светлой темы

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -33,7 +33,7 @@ export const Login = () => {
     return (
         <section>
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 bg-gray-800 border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Вход
@@ -46,7 +46,7 @@ export const Login = () => {
                             <div className="text-left">
                                 <label
                                     htmlFor="email"
-                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    className="block mb-2 text-sm font-medium dark:text-white text-white"
                                 >
                                     почта
                                 </label>
@@ -55,7 +55,7 @@ export const Login = () => {
                                     name="email"
                                     id="email"
                                     onChange={(e) => setEmail(e.target.value)}
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="name@company.com"
                                     required=""
                                 />
@@ -63,7 +63,7 @@ export const Login = () => {
                             <div className="text-left">
                                 <label
                                     htmlFor="password"
-                                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                                    className="block mb-2 text-sm font-medium dark:text-white text-white"
                                 >
                                     пароль
                                 </label>
@@ -75,13 +75,13 @@ export const Login = () => {
                                         setPassword(e.target.value)
                                     }
                                     placeholder="••••••••"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     required=""
                                 />
                             </div>
                             <button
                                 type="submit"
-                                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-700 dark:hover:bg-sky-800 cursor-pointer"
+                                className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-sky-700 dark:hover:bg-sky-800 cursor-pointer bg-sky-700 hover:bg-sky-800"
                             >
                                 Войти
                             </button>
--- src/components/ui/Pagination.jsx
+++ src/components/ui/Pagination.jsx
@@ -16,7 +16,7 @@ export const Pagination = ({ usersPerPage, totalUsers, paginate }) => {
                             onClick={() => paginate(number)}
                             key={number}
                         >
-                            <li className="border-2 border-gray-300 px-7 py-2 rounded-full hover:bg-emerald-400 text-black  dark:bg-white dark:hover:bg-cyan-700">
+                            <li className="text-white border-2 border-gray-300 px-7 py-2 rounded-full  bg-amber-800 hover:bg-amber-600 ">
                                 {number}
                             </li>
                         </a>
@@ -43,7 +43,7 @@ export const PaginationTabel = ({ monthPerPage, totalMonth, paginate }) => {
                             onClick={() => paginate(number)}
                             key={number}
                         >
-                            <li className="border-2 border-gray-300 px-6 py-3 rounded-full hover:bg-emerald-400 text-white dark:bg-amber-800 dark:hover:bg-amber-600">
+                            <li className="text-white g-amber-800 hover:bg-amber-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-amber-800  dark:bg-amber-800 dark:hover:bg-amber-600  dark:border-gray-700">
                                 {number}
                             </li>
                         </a>
@@ -71,7 +71,7 @@ export const PaginationWeek = ({ weekPerPage, totalWeek, paginateWeek }) => {
                             onClick={() => paginateWeek(number)}
                             key={number}
                         >
-                            <li className="border-2 border-gray-300 px-5  py-2 rounded-full text-white dark:bg-amber-800 dark:hover:bg-amber-600">
+                            <li className="text-white g-amber-800 hover:bg-amber-600 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 bg-amber-800  dark:bg-amber-800 dark:hover:bg-amber-600  dark:border-gray-700">
                                 {number}
                             </li>
                         </a>
