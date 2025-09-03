73a0fef23063c22f68b9b3e717178c7935699199
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-09-03 10:23:49 +0500

    исправил стиль для кнопки

--- src/components/ui/AllShowUsers.jsx
+++ src/components/ui/AllShowUsers.jsx
@@ -159,7 +159,7 @@ export const AllShowUsers = ({ show, setShow }) => {
             <Cards />
             <Button
                 variant="outlined"
-                className="bg-emerald-500 p-2 text-white border-0"
+                className="text-white bg-gray-800 hover:bg-gray-600 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-amber-800 dark:hover:bg-amber-600 dark:focus:ring-gray-700 dark:border-gray-700"
                 onClick={() => {
                     setShow(false);
                 }}
