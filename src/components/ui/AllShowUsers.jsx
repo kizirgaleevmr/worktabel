6870ccb520bd979d3e6d9c3b7f8f58229d3bc03d
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-09-05 22:02:01 +0500

    Изменил расположение кнопки

--- src/components/ui/AllShowUsers.jsx
+++ src/components/ui/AllShowUsers.jsx
@@ -92,7 +92,7 @@ export const AllShowUsers = ({ show, setShow }) => {
     // для генерации карт с сотрудниками
     const Cards = () => {
         if (loading) {
-            return <h2 className="text-white text-2xl">Loading...</h2>;
+            return <h2 className="text-white text-2xl mb-10">Loading...</h2>;
         }
         return (
             <div>
@@ -145,6 +145,15 @@ export const AllShowUsers = ({ show, setShow }) => {
                     totalUsers={users.length}
                     paginate={paginate}
                 />
+                <Button
+                    variant="outlined"
+                    className="text-white bg-amber-800 hover:bg-amber-600 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-amber-800 dark:hover:bg-amber-600 dark:focus:ring-gray-700 dark:border-gray-700"
+                    onClick={() => {
+                        setShow(false);
+                    }}
+                >
+                    Закрыть
+                </Button>
             </div>
         );
     };
@@ -157,15 +166,6 @@ export const AllShowUsers = ({ show, setShow }) => {
         >
             {/* показ всех ползователей в card */}
             <Cards />
-            <Button
-                variant="outlined"
-                className="text-white bg-amber-800 hover:bg-amber-600 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-amber-800 dark:hover:bg-amber-600 dark:focus:ring-gray-700 dark:border-gray-700"
-                onClick={() => {
-                    setShow(false);
-                }}
-            >
-                Закрыть
-            </Button>
             {userObject ? (
                 <DrawerPlacement
                     openRight={openRight}
