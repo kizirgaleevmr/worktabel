f989bd0b51229a4e6bbc581904ebd3a7fcc26f46
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-09-07 14:55:53 +0500

    добавил. relative

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,23 @@
+import MySlider from "../components/ui/Swiperr/Swiper";
 import { DefaultSidebar } from "../components/ui/Sidebar";
-
+import { useLocation } from "react-router-dom";
+import React, { useState, useEffect } from "react";
 export const Home = () => {
+    const location = useLocation();
+    const currentPath = location.pathname; // Текущий путь
+    const [pathState, setPathState] = useState();
+
+    React.useEffect(() => {
+        if (currentPath !== "/home") {
+            setPathState(false);
+        } else {
+            setPathState(true);
+        }
+    }, [currentPath]);
     return (
-        <section id="home">
+        <section id="home" className="relative">
+            {pathState && <MySlider />}
             <DefaultSidebar />
-            {/* <h1>Слайдер о компнаии</h1> */}
         </section>
     );
 };
