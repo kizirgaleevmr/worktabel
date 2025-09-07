fd425b027a0c2b905f4963d0feac849b061c5666
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-09-07 14:56:44 +0500

    добавил отступ  и компонент таблицы

--- src/pages/Message.jsx
+++ src/pages/Message.jsx
@@ -1,7 +1,8 @@
-import { getTodos } from "../components/utils/getEqupment";
 import React, { useEffect, useState } from "react";
 import { FormAddMessage } from "../components/ui/Form/FormAddMessage";
 import { fetchUsers } from "../components/config/firebase";
+import { Table } from "../components/ui/Table/Table";
+
 export const Message = () => {
     const [users, setUsers] = useState([]);
     const [user, setUser] = useState(null);
@@ -22,8 +23,8 @@ export const Message = () => {
     }, [users]);
 
     return (
-        <>
-            <header>
+        <div className="">
+            <header className="mb-6">
                 <section id="createMessage" className="mb-4 text-left">
                     <FormAddMessage title="Создать заявку" user={user} />
                 </section>
@@ -32,11 +33,19 @@ export const Message = () => {
                 </section>
             </header>
             <main>
-                <h3 className="text-left text-white">
+                <h3 className="text-left text-white mb-6">
                     ЗДесь будет таблица с заявками
                 </h3>
+                <Table />
+                <hr />
+                <p className="text-white text-3xl">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dolorum vero suscipit dignissimos harum illo. Repellat eos
+                    praesentium iusto rerum, in saepe inventore laudantium,
+                    fugit, necessitatibus nulla accusamus esse omnis expedita!
+                </p>
             </main>
             {/* <MyComp /> */}
-        </>
+        </div>
     );
 };
