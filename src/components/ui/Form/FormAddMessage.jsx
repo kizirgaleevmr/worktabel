a3c1326dfbf8336d7f6513e3a8ac0c85286daf7a
Marsel Kizirgaleev <mkrmarsel@gmail.com>
2025-09-07 22:48:36 +0500

    удалил лог и неиспользуемый элемент

--- src/components/ui/Form/FormAddMessage.jsx
+++ src/components/ui/Form/FormAddMessage.jsx
@@ -370,11 +370,6 @@ export const FormAddMessage = ({ children, title, user }) => {
                                                                                         e,
                                                                                         index
                                                                                     ) => {
-                                                                                        {
-                                                                                            console.log(
-                                                                                                e
-                                                                                            );
-                                                                                        }
                                                                                         return (
                                                                                             <input
                                                                                                 key={
@@ -462,22 +457,6 @@ export const FormAddMessage = ({ children, title, user }) => {
                                     type="submit"
                                 />
                             </div>
-                            {/* <label
-                                htmlFor="username-success"
-                                className="block mb-2 text-sm font-medium text-green-700 dark:text-green-500"
-                            >
-                                Your name
-                            </label>
-                            <input
-                                type="text"
-                                id="username-success"
-                                className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700 dark:placeholder-green-500 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
-                                placeholder="Bonnie Green"
-                            />
-                            <p className="mt-2 text-sm text-green-600 dark:text-green-500">
-                                <span className="font-medium">Alright!</span>{" "}
-                                Username available!
-                            </p> */}
                         </div>
                         {/* <div class="flex">
                         <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
